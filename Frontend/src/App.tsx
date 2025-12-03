177ca46f0924801e9304f4ed92d4250dfabab745
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-12-03 10:24:33 +0100

    header

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,9 @@
+import Header from "./Header";
+
 function App() {
   return (
-    <div style={{ height: "100%", backgroundColor: "#121212" }}>
-      <header>
-        <h2>Recor Label Application</h2>
-      </header>
+    <div style={{ height: "100vh", backgroundColor: "#121212" }}>
+      <Header />
     </div>
   );
 }
