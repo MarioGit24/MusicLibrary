fae117aa7a59a2991ec3219e911cf4b6e0754230
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-12-13 17:08:28 +0100

    some styling

--- Frontend/src/menu/Modal.tsx
+++ Frontend/src/menu/Modal.tsx
@@ -1,19 +1,22 @@
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Fade from "@mui/material/Fade";
 import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 const style = {
   position: "absolute",
-  top: "50%",
+  top: "45%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "80%",
   bgcolor: "#1f1f1f",
   borderRadius: "5px",
-  boxShadow: 24,
-  p: 4,
+  boxShadow: 10,
+  p: 3,
 };
 
 interface Props {
@@ -24,6 +27,23 @@ interface Props {
 
 function ModalMenu({ open, setOpen, type }: Props) {
   const handleClose = () => setOpen(false);
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(formData);
+    // Add form submission logic here
+  };
 
   return (
     <div>
@@ -46,13 +66,63 @@ function ModalMenu({ open, setOpen, type }: Props) {
               id="transition-modal-title"
               variant="h6"
               component="h2"
-              style={{ color: "#8F6D92" }}
+              style={{ color: "#9e79a1ff" }}
             >
-              Add {type}
+              Create {type}
             </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <Typography
+              id="transition-modal-description"
+              sx={{ mt: 1, fontSize: 13 }}
+            >
+              {type === "album"
+                ? "Create an album with songs"
+                : type === "EP"
+                ? "Create an EP with songs"
+                : ""}
             </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            >
+              <TextField
+                placeholder="Add a name..."
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                fullWidth
+                sx={{
+                  mt: 2,
+                  background: "#d7d7d718",
+                  borderRadius: 2,
+                  input: {
+                    color: "#aea5afff",
+                    "&::placeholder": {
+                      color: "#8e7990e4",
+                    },
+                  },
+                  fieldset: { borderColor: "#aea5af23" },
+                  "& .MuiOutlinedInput-root": {
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#aea5af23",
+                    },
+                  },
+                }}
+              />
+              <div
+                style={{ display: "flex", justifyContent: "end", marginTop: 5 }}
+              >
+                <Button
+                  type="submit"
+                  style={{
+                    color: "#8F6D92",
+                    background: "#16161693",
+                  }}
+                >
+                  Create
+                </Button>
+              </div>
+            </Box>
           </Box>
         </Fade>
       </Modal>
