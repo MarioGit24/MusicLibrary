0684a16fdae71f1fe00412481baa9cc226c5537c
viggo2003 <vignie1217@skola.goteborg.se>
2025-12-18 13:49:45 +0100

    added readme

---
+++ README.sh
@@ -0,0 +1,5 @@
+1. Run the script (./run.sh) to start all microservices and bring up our site. (if it doesnt open the site, the address is http://localhost:3000/)
+2. press the plus icon to add an artist.
+3. press the plus icon to add a release (EP, Single, Album).
+4. select the one you want and fill out the fields. At the bottom you are able to select an artist youve created.
+5. Click the album created under the "Releases" label on the site to get a more detailed view of the release.
